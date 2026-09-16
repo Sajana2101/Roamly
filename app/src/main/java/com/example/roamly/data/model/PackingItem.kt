b0b6c97ecfed5e05940baddee994bf04902da2ca
Nabihah Sema <ST10440590@myemeris.edu.za>
2026-09-16 09:36:27 +0200

    Added attributes for the 'Packing' page

--- app/src/main/java/com/example/roamly/data/model/PackingItem.kt
+++ app/src/main/java/com/example/roamly/data/model/PackingItem.kt
@@ -1,3 +1,13 @@
 package com.example.roamly.data.model
 
-data class PackingItem()
+data class PackingItem(
+
+    // represents 1 item inside a packing list
+
+    val packingItemId: String,
+    val packingListId: String,
+    val name: String,
+    val isPacked: Boolean,
+    val createdAt: String,
+    val updatedAt: String
+)
