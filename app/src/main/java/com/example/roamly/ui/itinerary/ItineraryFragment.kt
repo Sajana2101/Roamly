cb509f44677e11edb950ef57a145665bb05d6a1b
ammaarahk05 <ST10447616@vcconnect.edu.za>
2026-09-17 08:45:42 +0200

    Built itinerary planner interface

---
+++ app/src/main/java/com/example/roamly/ui/itinerary/ItineraryFragment.kt
@@ -0,0 +1,15 @@
+package com.example.roamly.ui.itinerary
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.roamly.R
+
+class ItineraryFragment : Fragment(R.layout.fragment_itinerary) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
