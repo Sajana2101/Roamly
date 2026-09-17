4577bb5934ded075f12a1a2b9bc6785ebd83c40f
Nabihah Sema <ST10440590@myemeris.edu.za>
2026-09-17 20:52:03 +0200

    Added Settings page

--- app/src/main/java/com/example/roamly/MainActivity.kt
+++ app/src/main/java/com/example/roamly/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.roamly
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.roamly.ui.packing.PackingFragment
+import com.example.roamly.ui.settings.SettingsFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,15 +16,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-            // just to display the pages without having the proper UIs yet
-        if (savedInstanceState == null) {
+
+        val packingButton = findViewById<Button>(R.id.btnPacking)
+        val settingsButton = findViewById<Button>(R.id.btnSettings)
+
+        // Open Packing page
+        packingButton.setOnClickListener {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.fragmentContainer, PackingFragment())
                 .commit()
         }
 
+        // Open Settings page
+        settingsButton.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, SettingsFragment())
+                .commit()
+        }
 
-
+//        // Open Packing page when app starts
+//        if (savedInstanceState == null) {
+//            supportFragmentManager.beginTransaction()
+//                .replace(R.id.fragmentContainer, PackingFragment())
+//                .commit()
+//        }
 
 
 
